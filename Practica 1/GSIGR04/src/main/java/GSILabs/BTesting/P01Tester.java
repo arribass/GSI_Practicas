05eb60dd73ca5b7309271a8863b8098e1089ffa5
Adrian <55694226+arribass@users.noreply.github.com>
2021-09-14 12:49:51 +0200

    Create P01Tester.java

---
+++ Practica 1/GSIGR04/src/main/java/GSILabs/BTesting/P01Tester.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GSILabs.BTesting;
+
+/**
+ *
+ * @author Arribas
+ */
+public class P01Tester {
+    public static void main(String[] args) {
+        
+    }
+}
