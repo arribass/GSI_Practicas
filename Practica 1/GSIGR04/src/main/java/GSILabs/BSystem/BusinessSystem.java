82e5393f8185e87535cbec8cbfcccdb60925dd1d
Adrian <adri47arribas@gmail.com>
2021-09-14 17:21:55 +0200

    Inherit Javadoc para BusinessSystem

--- Practica 1/GSIGR04/src/main/java/GSILabs/BSystem/BusinessSystem.java
+++ Practica 1/GSIGR04/src/main/java/GSILabs/BSystem/BusinessSystem.java
@@ -44,141 +44,225 @@ public class BusinessSystem implements LeisureOffice{
         return validarLongitudNick(u.getNick()) && !existeNick(u.getNick());
     }
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean eliminaUsuario(Usuario u) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean modificaUsuario(Usuario u, Usuario nuevoU) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean existeNick(String nick) {
         return Usuarios.stream().anyMatch(item -> nick.equals(item.getNick()));
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Usuario obtenerUsuario(String nick) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean nuevaReview(Review r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean eliminaReview(Review r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean existeRewiew(Usuario u, Local l, LocalDate ld) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean nuevaContestacion(Contestacion c, Review r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean tieneContestacion(Review r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Contestacion obtenerContestacion(Review r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean eliminaContestacion(Contestacion c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean nuevoLocal(Local l) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean eliminarLocal(Local l) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Local obtenerLocal(Direccion d) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean asociarLocal(Local l, Propietario p) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean desasociarLocal(Local l, Propietario p) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean actualizarLocal(Local viejoL, Local nuevoL) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Review[] verReviews(Local l) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean nuevaReserva(Cliente c, Reservable r, LocalDate ld, LocalTime lt) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Reserva[] obtenerReservas(Cliente c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Reserva[] obtenerReservas(Reservable r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Reserva[] obtenerReservas(LocalDate ld) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean eliminarReserva(Reserva r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Local[] listarLocales(String ciudad, String provincia) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Bar[] listarBares(String ciudad, String provincia) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Restaurante[] listarRestaurantes(String ciudad, String provincia) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public Pub[] listarPubs(String ciudad, String provincia) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean eliminaContestacion(Review r) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
