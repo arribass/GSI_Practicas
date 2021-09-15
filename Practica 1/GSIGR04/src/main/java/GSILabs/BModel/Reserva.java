6a48af52c4e3d23bc7b8a161a0d9b3a862ef3371
Sergio llanas <78312450+SergioLlanas@users.noreply.github.com>
2021-09-15 11:43:13 +0200

    rellenada clase reserva

--- Practica 1/GSIGR04/src/main/java/GSILabs/BModel/Reserva.java
+++ Practica 1/GSIGR04/src/main/java/GSILabs/BModel/Reserva.java
@@ -5,10 +5,22 @@
  */
 package GSILabs.BModel;
 
+import java.util.Date;
+
 /**
  *
  * @author Arribas
  */
 public class Reserva {
+    private Date fecha;
+    private String hora;
+    private float descuento;
     
+    public Reserva(Date fecha, String hora, float descuento){
+        this.descuento = descuento;
+        this.fecha = fecha;
+        //Si tengo un 25% de descuento calcularia 0.85 que es por lo que tengo que multiplicar el precio para que me salga un 25 de descuento.
+        //Ejemplo: vale 10 euros -> 10 * 0.85 = 8.5
+        this.descuento = 1 - (descuento/100); 
+    }
 }
