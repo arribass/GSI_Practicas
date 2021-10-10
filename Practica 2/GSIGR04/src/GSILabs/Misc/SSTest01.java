0b27f1c673860c128e784e946ef8fd8cb0e6241b
sapotomate <pandecenteno1@gmail.com>
2021-10-10 17:27:14 +0200

    Primer y segundo ejercicio
    Hay que conseguir que el segundo ejercicio genere el archivo sin nombres en las columnas

---
+++ Practica 2/GSIGR04/src/GSILabs/Misc/SSTest01.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GSILabs.Misc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import org.jopendocument.dom.OOUtils;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
+
+/**
+ *
+ * @author Asier
+ */
+public class SSTest01 {
+    
+    public static void main(String[] args) throws IOException {
+
+        final Object[][] array = new Object[4][6];
+        
+        Random random = new Random();
+        for(int i = 0; i<4; i++){
+            for(int j = 0; j<6; j++){
+                int randInt = random.nextInt(100);
+                array[i][j] = (int) randInt;
+                System.out.println(array[i][j]);
+            }
+        }
+        String[] columns = new String[] { "a", "b", "c", "d", "e", "f" };
+        TableModel model = new DefaultTableModel(array, columns);
+        final File file = new File("test01.ods");
+        SpreadSheet.createEmpty(model).saveAs(file);
+        OOUtils.open(file);
+    }
+}
