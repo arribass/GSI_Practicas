ab09a1ec5fe4096a50d49157b72f1d3dde0855fb
SergioLlanas <78312450+SergioLlanas@users.noreply.github.com>
2021-11-19 14:29:36 +0100

    creados ClientHub y AdminHub y relleandos (sin probar funcionamiento) ejercicio 3

---
+++ Practica 4/GSIGR04/src/GSILabs/connect/AdminHub.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GSILabs.connect;
+
+import GSILabs.BModel.Direccion;
+import GSILabs.BModel.Local;
+import GSILabs.BModel.Review;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author sergi
+ */
+public class AdminHub {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[])
+    {
+        try	
+        {
+            System.out.println("¿A qué dirección te quieres conectar?");
+            String direccion = sc.nextLine();
+            System.out.println("¿Qué puerto quieres utilizar?");
+            int puerto = sc.nextInt();
+            System.out.println("¿Cuál es el tag del objeto?");
+            String tag = sc.nextLine();
+            //Instanciar el registro RMI
+            Registry registro=LocateRegistry.getRegistry(puerto);	
+            //Instanciar un objeto de la clase del servidor
+            AdminGateway ag=(AdminGateway) registro.lookup(tag);	
+            //Uso del servicio
+            Direccion d = new Direccion("Haro", "La Rioja", "Avenida mayor", "3");
+            Local l = new Local("Casa Paco", d, "Bar elegante en el centro.");
+            ag.eliminaLocal(l);
+            
+        }catch (Exception e)
+        {
+            System.err.println("Excepción en el ClientHub:");
+            e.printStackTrace();
+        }
+    }
+}
